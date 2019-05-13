c2a29690aa7028813e5eeb2baba9375c1705e8d3
RodBromberg <rod.bromberg@yahoo.com>
2019-05-12 21:25:41 -0400

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: { filename: './data/lambda.db3' },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-      tableName: 'dbmigrations',
+    development: {
+        client: 'sqlite3',
+        connection: { filename: './data/lambda.db3' },
+        useNullAsDefault: true,
+        migrations: {
+            directory: './data/migrations',
+            tableName: 'dbmigrations',
+        },
+        seeds: { directory: './data/seeds' },
     },
-    seeds: { directory: './data/seeds' },
-  },
 };
+
+//hello
